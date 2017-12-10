072b6a8320541d943782f20b8334a66765597c58
lecu1012 <lecu1012@gmail.com>
2017-12-10 02:57:49 +0000

    init

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: "example.com",
+    domain: "heroku.com",
     address: "smtp.SendGrid.net",
     port: 587,
     authentication: :plain,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
-   {
+  {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: "heroku.com",
@@ -101,5 +101,5 @@ Rails.application.configure do
     port: 587,
     authentication: :plain,
     enable_starttls_auto: true
-   }
- end
+  }
+end
