80569753730a0b1ac120b8e6ffe8d935911a3cf8
lecu1012 <lecu1012@gmail.com>
2017-12-10 02:49:09 +0000

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'still-dusk-76021.herokuapp.com/' }
+
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
    {
